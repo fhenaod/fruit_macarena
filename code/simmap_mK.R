af957592dd9a5186fdcdab1841e8aa78286ee5fe
Francisco <fhenaod@zoology.ubc.ca>
2020-05-18 00:13:21 -0700

    neves et al. genera phylo, reorder code, loading results from cluster

--- code/simmap_mK.R
+++ code/simmap_mK.R
@@ -3,15 +3,15 @@ library(treeplyr)
 library(bayou)
 
 # Data housekeeping ####
-tree <- read.tree("data/tree_calib.nwk")
+#tree <- read.tree("data/tree_calib.nwk")
 tree <- read.tree("data/Genus_phylogeny.tre")
-tree$tip.label <- tolower(tree$tip.label)
+tree$tip.label <- (tree$tip.label)
 fruit_dat_raw <- read.csv("data/Base plantas Tinigua modificable_Agosto.csv", header = T )
-fruit_dat_raw$ESPECIE <- tolower(fruit_dat_raw$ESPECIE)
+fruit_dat_raw$ESPECIE <- (fruit_dat_raw$ESPECIE)
 
 fruit_dat_raw$Genus <- (sapply(strsplit(as.character(fruit_dat_raw$ESPECIE), split = "_", fixed = T), "[", 1))
 
-fruit_dat_raw <- filter(fruit_dat_raw, !is.na(LARGO_FRUTO.cm.))
+#fruit_dat_raw <- filter(fruit_dat_raw, !is.na(LARGO_FRUTO.cm.))
 fruit_dat_raw <- filter(fruit_dat_raw, !is.na(Sistema_de_Dispersion))
 
 fruit_dat <- fruit_dat_raw %>% select(Genus, ESPECIE,
@@ -24,7 +24,8 @@ fruit_dat <- fruit_dat_raw %>% select(Genus, ESPECIE,
                                       Densidad..specific.gravity.,
                                       Tipo.de.hoja,
                                       Sistema_de_Dispersion,
-                                      Color.Janson) 
+                                      Color.Janson,
+                                      Habito) 
 
 tree_data <- make.treedata(tree, fruit_dat)
 tree_data <- tree_data %>%  mutate(fr_len = log(LARGO_FRUTO.cm.), fr_wd = log(Fr.width..cm.),
@@ -40,46 +41,7 @@ tree_data <- tree_data %>% filter(Sistema_de_Dispersion %in% c("Anemocorica", "D
 
 tree_data$dat <- droplevels(tree_data$dat)
 summary(getVector(tree_data, Sistema_de_Dispersion))
-
-# Simulate stochastic character maps ####
-
-disp_simp_er <- make.simmap(tree_data$phy, getVector(tree_data, Sistema_de_Dispersion), model = "ER", nsim = 1000)
-saveRDS(disp_simp_er, "output/disp_simp_er.rds")
-disp_simp_ard <- make.simmap(tree_data$phy, getVector(tree_data, Sistema_de_Dispersion), model = "ARD", nsim = 1000)
-saveRDS(disp_simp_ard, "ou:tput/disp_simp_ard.rds")
-disp_simp_sym <- make.simmap(tree_data$phy, getVector(tree_data, Sistema_de_Dispersion), model = "SYM", nsim = 1000)
-saveRDS(disp_simp_sym, "output/disp_simp_sym.rds")
-
-disp_simp_er <- readRDS("output/disp_simp_er.rds")
-disp_simp_ard <- readRDS("output/disp_simp_ard.rds")
-disp_simp_sym <- readRDS("output/disp_simp_sym.rds")
-
-disp_simp_er_sum <- summary(disp_simp_er, plot = F)
-disp_simp_ard_sum <- summary(disp_simp_ard, plot = F)
-disp_simp_sym_sum <- summary(disp_simp_sym, plot = F)
-
-cols <- setNames(palette()[1:length(levels(getVector(tree_data, Sistema_de_Dispersion)))],levels(getVector(tree_data, Sistema_de_Dispersion)))
-
-# Equal Rates
-plot(disp_simp_er_sum, cols, type = "fan", fsize = .01, lwd = .5, cex = c(.4,.2))
-add.simmap.legend(colors = cols, x = 0.95*par()$usr[1],y = 0.9*par()$usr[4],prompt = FALSE, fsize = 0.6)
-
-disp_simp_er_dd <- density(disp_simp_er)
-plot(disp_simp_er_dd)
-
-# All-Rates-Different
-plot(disp_simp_ard_sum, cols, type = "fan", fsize = .01, lwd = .5, cex = c(.4,.2))
-add.simmap.legend(colors = cols, x = 0.95*par()$usr[1], y = 0.9*par()$usr[4], prompt = FALSE, fsize = 0.6)
-
-disp_simp_ard_dd <- density(disp_simp_ard)
-plot(disp_simp_ard_dd)
-
-# Symmetrical model
-plot(disp_simp_sym_sum, cols, type = "fan", fsize = .01, lwd = .5, cex = c(.4,.2))
-add.simmap.legend(colors = cols, x = 0.95*par()$usr[1], y = 0.9*par()$usr[4], prompt = FALSE, fsize = 0.6)
-
-disp_simp_sym_dd <- density(disp_simp_sym)
-plot(disp_simp_sym_dd)
+summary(getVector(tree_data, Habito))
 
 # Mk models ####
 disp_er <- fitMk(tree_data$phy, getVector(tree_data, Sistema_de_Dispersion), model = "ER")
@@ -89,18 +51,63 @@ saveRDS(disp_ard, "output/disp_ard.rds")
 disp_sym <- fitMk(tree_data$phy, getVector(tree_data, Sistema_de_Dispersion), model = "SYM")
 saveRDS(disp_sym, "output/disp_sym.rds")
 
-disp_er <- readRDS("output/disp_er.rds")
-disp_ard <- readRDS("output/disp_ard.rds") 
-disp_sym <- readRDS("output/disp_sym.rds")
+disp_er <- readRDS("dispersal_Mk/output/disp_er.rds")
+disp_ard <- readRDS("dispersal_Mk/output/disp_ard.rds") 
+disp_sym <- readRDS("dispersal_Mk/output/disp_sym.rds")
 
 mod_sum <- data.frame(Mk_model = c("ARD","SYM","ER"),
-           logLik = c(logLik(disp_ard), logLik(disp_sym), logLik(disp_er)),
-           k = c(attr(AIC(disp_ard),"df"), attr(AIC(disp_sym),"df"), attr(AIC(disp_er),"df")),
-           AIC = c(AIC(disp_ard), AIC(disp_sym), AIC(disp_er)),
-           ΔAIC = round(qpcR::akaike.weights(c(AIC(disp_ard), AIC(disp_sym), AIC(disp_er)))$deltaAIC, 2),
-           AICw = round(qpcR::akaike.weights(c(AIC(disp_ard), AIC(disp_sym), AIC(disp_er)))$weights, 2)
-           )
+                      logLik = c(logLik(disp_ard), logLik(disp_sym), logLik(disp_er)),
+                      k = c(attr(AIC(disp_ard),"df"), attr(AIC(disp_sym),"df"), attr(AIC(disp_er),"df")),
+                      AIC = c(AIC(disp_ard), AIC(disp_sym), AIC(disp_er)),
+                      ΔAIC = round(qpcR::akaike.weights(c(AIC(disp_ard), AIC(disp_sym), AIC(disp_er)))$deltaAIC, 2),
+                      AICw = round(qpcR::akaike.weights(c(AIC(disp_ard), AIC(disp_sym), AIC(disp_er)))$weights, 2)
+)
+mod_sum
 
-plot(disp_er, show.zeros = F, main = "Equal rates")
+#plot(disp_er, show.zeros = F, main = "Equal rates")
 plot(disp_ard, show.zeros = F, main = "All-rates different")
-plot(disp_sym, show.zeros = F, main = "Symmetrical rates")
+#plot(disp_sym, show.zeros = F, main = "Symmetrical rates")
+
+# Simulate stochastic character maps ####
+disp_simp_er <- make.simmap(tree_data$phy, getVector(tree_data, Sistema_de_Dispersion), model = "ER", nsim = 1000)
+saveRDS(disp_simp_er, "output/disp_simp_er.rds")
+disp_simp_ard <- make.simmap(tree_data$phy, getVector(tree_data, Sistema_de_Dispersion), model = "ARD", nsim = 1000)
+saveRDS(disp_simp_ard, "output/disp_simp_ard.rds")
+disp_simp_sym <- make.simmap(tree_data$phy, getVector(tree_data, Sistema_de_Dispersion), model = "SYM", nsim = 1000)
+saveRDS(disp_simp_sym, "output/disp_simp_sym.rds")
+
+#disp_simp_er <- readRDS("dispersal_Mk/output/disp_simp_er.rds")
+disp_simp_ard <- readRDS("dispersal_Mk/output/disp_simp_ard.rds")
+#disp_simp_sym <- readRDS("dispersal_Mk/output/disp_simp_sym.rds")
+
+disp_simp_er_sum <- summary(disp_simp_er, plot = F)
+disp_simp_ard_sum <- summary(disp_simp_ard, plot = F)
+disp_simp_sym_sum <- summary(disp_simp_sym, plot = F)
+
+#disp_simp_er_sum <- readRDS("dispersal_Mk/output/disp_er_sum.rds")
+disp_simp_ard_sum <- readRDS("dispersal_Mk/output/disp_ard_sum.rds")
+#disp_simp_sym_sum <- readRDS("dispersal_Mk/output/disp_sym_sum.rds")
+
+cols <- setNames(palette()[1:length(levels(getVector(tree_data, Sistema_de_Dispersion)))], levels(getVector(tree_data, Sistema_de_Dispersion)))
+
+# Equal Rates
+#plot(disp_simp_er_sum, cols, type = "fan", fsize = .01, lwd = .5, cex = c(.4,.2))
+#add.simmap.legend(colors = cols, x = 0.95*par()$usr[1],y = 0.9*par()$usr[4],prompt = FALSE, fsize = 0.6)
+
+#disp_simp_er_dd <- density(disp_simp_er)
+#plot(disp_simp_er_dd)
+
+# All-Rates-Different
+plot(disp_simp_ard_sum, cols, type = "fan", fsize = .01, lwd = .5, cex = c(.4,.2))
+add.simmap.legend(colors = cols, x = 0.95*par()$usr[1], y = 0.9*par()$usr[4], prompt = FALSE, fsize = 0.6)
+head(disp_simp_ard_sum$ace)
+
+disp_simp_ard_dd <- density(disp_simp_ard)
+plot(disp_simp_ard_dd)
+
+# Symmetrical model
+#plot(disp_simp_sym_sum, cols, type = "fan", fsize = .01, lwd = .5, cex = c(.4,.2))
+add.simmap.legend(colors = cols, x = 0.95*par()$usr[1], y = 0.9*par()$usr[4], prompt = FALSE, fsize = 0.6)
+
+#disp_simp_sym_dd <- density(disp_simp_sym)
+#plot(disp_simp_sym_dd)
